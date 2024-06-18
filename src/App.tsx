abd4fc25f35a495ae5f1f8e20b3eab1e0ad429a4
nikitabiichuk2009 <niktestpython@gmail.com>
2024-06-18 15:05:25 +0300

    Small fixes of setup with React, TypeScript, Chakra UI, Prettier, and ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import logo from './logo.svg'
 import './App.css'
+import { Box, Code, Heading, Image, Link, Text } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+    <Box as="div" className="App">
+      <Box as="header" className="App-header">
+        <Image as="img" src={logo} className="App-logo" alt="logo" />
+        <Text as={'p'}>
+          Edit <Code as="code">src/App.tsx</Code> and save to reload.
+        </Text>
+        <Link
+          as="a"
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+        <Heading as="h1">Hello, world!</Heading>
+      </Box>
+    </Box>
   )
 }
 
